c8535999eb0ee2463d6625290cc5dbe50168a480
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-07 09:47:20 -0300

    :tada: Begin a project

---
+++ src/pages/Article/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles"
+
+const Article: React.FC = () => {
+  return (
+    <S.ArticleWrapper></S.ArticleWrapper>
+  )
+}
+
+export default Article
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles"
+
+const Main: React.FC = () => {
+  return (
+    <S.MainWrapper></S.MainWrapper>
+  )
+}
+
+export default Main
