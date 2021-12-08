778c779de00a95ee24c4da0e4e4a935d936c0e20
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-08 15:05:58 -0300

    💄 Some style changes

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -4,4 +4,5 @@ export const ContainerWrapper = styled.div`
   width: 100%;
   margin: 0 auto;
   max-width: 1200px;
+  padding: ${({ theme }) => theme.spacing.gap1} ${({ theme }) => theme.spacing.gap3};
 `
