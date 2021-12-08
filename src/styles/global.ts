28d7d70a3b3c64e210dadfeef1af08978bbb10f5
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-07 21:29:51 -0300

    💄 Added some resets

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,9 +5,23 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
   }
 
   body {
-    background-color: red;
+    width: 100%;
+    background-color: #050607;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    color: #FFFF;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
   }
 `
