68c398fda9e911a218ddd230c066702b3a079884
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-08 12:07:09 -0300

    💄 Finished the Header component styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import * as S from "./styles"
+import Container from "../Container";
+import Input from "../Input";
+import Logo from "../Logo";
+import * as S from "./styles";
 
 const Header: React.FC = () => {
-  return <S.HeaderWrapper></S.HeaderWrapper>;
+  return (
+    <Container>
+    <S.HeaderWrapper>
+        <Logo />
+        <Input type="text" name="search"placeholder="Search"/>
+        <S.HeaderNav>
+          <ul>
+            <li><a href="#categories">Categories</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#content">Content</a></li>
+          </ul>
+        </S.HeaderNav>
+    </S.HeaderWrapper>
+    </Container>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
-export const HeaderWrapper = styled.header``
+export const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${({ theme }) => theme.spacing.gap1} ${({ theme }) => theme.spacing.gap3};
+`;
+
+export const HeaderNav = styled.nav`
+  ul {
+    display: flex;
+
+    li {
+      margin-left: ${({ theme }) => theme.spacing.gap3};
+
+      a {
+        display: inline-block;
+        transition: ease-in-out 0.3s;
+        padding: ${({ theme }) => theme.spacing.gap1};
+        color: ${({ theme }) => theme.colors.whiteDark};
+
+        &:hover {
+          opacity: 0.7;
+        }
+      }
+    }
+  }
+`;
