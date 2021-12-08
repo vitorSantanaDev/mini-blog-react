237d266dd60cd49c4b653602c7bf973871076c89
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-08 15:06:26 -0300

    💄 added styles

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,10 +1,37 @@
 import React from "react";
-import * as S from "./styles"
+import Container from "../Container";
+import * as S from "./styles";
 
-const Post: React.FC = () => {
-  return(
-    <S.PostWrapper></S.PostWrapper>
-  )
+interface PostProps {
+  title: string
+  subtitle: string
 }
 
-export default Post
+const Post: React.FC<PostProps> = ({ title, subtitle }) => {
+  return (
+    <Container>
+      <S.PostWrapper>
+        <S.PostContent>
+          <div className="imagePost"></div>
+          <h5>{title}</h5>
+          <h3>{subtitle}</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+            sit, officiis commodi dicta cum est laborum! Ab quod praesentium sit
+            quaerat pariatur aliquid assumenda porro! Optio corporis fugit
+            ratione nam?
+          </p>
+        </S.PostContent>
+        <S.PostProfileData>
+          <div className="imageProfile"></div>
+          <div className="dataProfile">
+            <h6>Fulano de Tal</h6>
+            <p>Aug 2, 2020 - 8 min read</p>
+          </div>
+        </S.PostProfileData>
+      </S.PostWrapper>
+    </Container>
+  );
+};
+
+export default Post;
--- src/components/Post/styles.ts
+++ src/components/Post/styles.ts
@@ -1,3 +1,51 @@
 import styled from "styled-components";
 
-export const PostWrapper = styled.div``
+export const PostWrapper = styled.div`
+  margin: ${({ theme }) => theme.spacing.gap2};
+`;
+
+export const PostContent = styled.div`
+  h5 {
+    font-weight: 300;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.colors.blueLight};
+    font-size: ${({ theme }) => theme.typograph.h5};
+  }
+
+  h3 {
+    font-weight: 300;
+    margin: ${({ theme }) => theme.spacing.gap1} 0;
+    font-size: ${({ theme }) => theme.typograph.h4};
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors.gray4};
+    font-size: ${({ theme }) => theme.typograph.p};
+  }
+`;
+
+export const PostProfileData = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: ${({ theme }) => theme.spacing.gap2};
+
+  .imageProfile {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    margin-right: ${({ theme }) => theme.spacing.gap2};
+    background-color: ${({ theme }) => theme.colors.gray2};
+  }
+
+  h6 {
+    font-weight: 300;
+    color: ${({ theme }) => theme.colors.blueNormal};
+    font-size: ${({ theme }) => theme.typograph.h6};
+  }
+
+  .dataProfile {
+    color: ${({ theme }) => theme.colors.gray4};
+    font-size: ${({ theme }) => theme.typograph.p};
+  }
+`;
