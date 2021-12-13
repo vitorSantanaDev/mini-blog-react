c46a5df9b3eaabd0253d9e18a25053b676f8734b
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-13 10:47:56 -0300

    ✨ Implementing the business rule on the Main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import defaultTheme from './styles/theme/defaultTheme';
+import Header from './components/Header';
+// import Articles from './components/Articles';
+import Main from './pages/Main';
 
 function App() {
   return (
    <ThemeProvider theme={defaultTheme}>
     <GlobalStyle />
+    <Header />
+    <Main />
    </ThemeProvider>
   );
 }
