e5946aa3ada23be18bbc3ebf20a4badf54d506a7
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-13 10:47:37 -0300

    ✨ Implementing the business rule on the Main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,55 @@
-import React from "react";
-import * as S from "./styles"
+import React, { useEffect, useState } from "react";
+import * as S from "./styles";
+import api from "../../services/api";
+import Post from "../../components/Post";
+import Container from "../../components/Container";
 
-const Main: React.FC = () => {
-  return (
-    <S.MainWrapper></S.MainWrapper>
-  )
+interface Posts {
+  category: string;
+  title: string;
+  description: string;
+  content: string;
+  author: string;
+  date: string;
+  id: string;
 }
 
-export default Main
+const Main: React.FC = () => {
+  const [posts, setPosts] = useState<Posts[]>();
+
+  useEffect(() => {
+    async function handleAxios() {
+      try {
+        const response = await api.get("/posts");
+        setPosts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    handleAxios();
+  }, []);
+
+  return (
+    <S.MainWrapper>
+      <Container className="container">
+        <h2>Articles</h2>
+        <S.ArticlePostsWrapper>
+          {posts
+            ? posts.map((post) => (
+                <Post
+                  key={post.id}
+                  title={post.category}
+                  subtitle={post.title}
+                  content={post.description}
+                  author={post.author}
+                  date={post.date}
+                />
+              ))
+            : null}
+        </S.ArticlePostsWrapper>
+      </Container>
+    </S.MainWrapper>
+  );
+};
+
+export default Main;
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export const MainWrapper = styled.div``
+export const MainWrapper = styled.div`
+  .container > h2 {
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.typograph.h2};
+  }
+`
+
+export const ArticlePostsWrapper = styled.div`
+  display: grid;
+  alingh-items: center;
+  justify-content: center;
+  grid-template-columns: repeat(3, 1fr);
+`
